247289bb1e9d3b5c9e82d80d0b6afe28ee4ab994
souldiall <souldogomet72@gmail.com>
2026-08-24 22:35:56 -0400

    add security groups resources

--- Terraform-Folders/Folder32-for_each-map/main.tf
+++ Terraform-Folders/Folder32-for_each-map/main.tf
@@ -1,11 +1,22 @@
+resource "aws_security_group" "sg" {
+  for_each = var.security_groups
+
+  tags = {
+    Name = each.key
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
 resource "aws_instance" "web" {
-  ami      = var.ami_id
-  for_each = var.names
+  for_each      = toset(var.instance_names)
+  ami           = var.iam_id
   instance_type = var.instance_type
 
   tags = {
     Name = each.key
   }
+  depends_on = [aws_security_group.sg]
   lifecycle {
     prevent_destroy = false
   }
