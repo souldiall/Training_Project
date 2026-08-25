9d91d75d9ae0f60cb15dbd42731f82341c571e6c
souldiall <souldogomet72@gmail.com>
2026-08-24 22:38:39 -0400

    Add security groups variable and update the file

--- Terraform-Folders/Folder32-for_each-map/variables.tf
+++ Terraform-Folders/Folder32-for_each-map/variables.tf
@@ -1,17 +1,22 @@
-variable "names" {
-  type = map(string)
+variable "security_groups" {
+  description = "Map of security group names to their intended purpose."
+  type        = map(string)
   default = {
-    "Instance1" = "yelow"
-    "Instance2" = "green"
-    "Instance3" = "blue"
-    "instance0" = "black"
+    "sg-web"  = "allo http, https traffic"
+    "sg-Admn" = "Access ssh access"
+    "sg-db"   = "Allow MYSQL traffic"
   }
-}
 
-variable "ami_id" {
+}
+variable "instance_names" {
+  type    = list(string)
+  default = ["web-1", "web-3"]
+}
+variable "iam_id" {
   type    = string
   default = "ami-0b6d9d3d33ba97d99"
 }
 variable "instance_type" {
+  type    = string
   default = "t3.micro"
 }
